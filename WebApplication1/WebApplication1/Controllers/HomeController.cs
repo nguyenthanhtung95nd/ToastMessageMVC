c65177b210f067274d63587db36cf9fb730204c0
DESKTOP-A8UILNG\nguyenthanhtung95nd <nguyenthanhtung95nd@gmail.com>
2018-10-29 20:59:52 +0700

    Init commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            this.AddToastMessage("LARION", "Nguyen Thanh Tung" + DateTime.Now.ToShortDateString(), ToastType.Info);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult GoSomewhereElse()
+
+        {
+
+            this.AddToastMessage("Redirected message", "This message has been redirected", ToastType.Info);
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/ToastType.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public enum ToastType
+    {
+        Error,
+        Info,
+        Success,
+        Warning
+    }
+}
